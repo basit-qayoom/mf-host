106be45eb4f3ec6e7783fe017c71a8ecda0e01e1
Basit Qayoom Chowdhary <basitqayoomchowdhary@Basit-12905.local>
2023-11-01 17:40:45 +0530

    intial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'MFHost',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+        },
+        remotes: {
+          MFRemote: `MFRemote@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+        },
+      })
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
